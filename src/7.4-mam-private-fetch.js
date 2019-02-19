2a94f0e9c07904bc0f2080dff113d43f336bfad0
solareenlo <solareenlo@gmail.com>
2019-02-19 21:39:18 +0900

    mamを使ってprivateメッセージをfetch

--- src/7.4-mam-private-fetch.js
+++ src/7.4-mam-private-fetch.js
@@ -2,23 +2,27 @@
 // MAM: Publish messages to Private Stream
 ///////////////////////////////
 
-const Mam = require('@iota/mam');
+// const Mam = require('@iota/mam');
+const Mam = require('../lib/mam.client.js')
 const { trytesToAscii } = require('@iota/converter');
 
 // Init State
-let root =
-  'QQUTOYQDKWGEOSNDERIUTLNFJLKXKPGMZVDJJJNPMSXSIYHJDUUMZUEI9GGQSFQJD9GRVGCMNDDATPWDN';
 const mamType = 'restricted';
-const mamSecret = 'DONTSHARETHIS';
+const mamSecret = 'FQYHOVFLTDQDZIIFJQTGDPYMOQFCJIT9GIMASFBLVMTKFUTGTIJ9Q9HYBNVNMWEUPBKWOQTKYLQYINYBZ';
 
 // Initialise MAM State
-let mamState = Mam.init('https://nodes.devnet.iota.org:443');
+const provider = 'https://private.tangle.jp:443'
+let mamState = Mam.init(provider);
 
 // Callback used to pass data out of the fetch
 const logData = data => console.log(trytesToAscii(data));
+
+if (!process.argv[2]) return console.log('No Address!');
+const root = process.argv[2];
 
 const execute = async () => {
   // Callback used to pass data + returns next_root
   const resp = await Mam.fetch(root, mamType, mamSecret, logData);
 };
+
 execute();
