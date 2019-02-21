7e7d4f4bb9a7c8206c3b69b017f164f0ad88d1ea
solareenlo <solareenlo@gmail.com>
2019-02-22 01:36:44 +0900

    動くようにした

--- src/7.5-mam-public-watch.js
+++ src/7.5-mam-public-watch.js
@@ -1,14 +1,20 @@
-const Mam = require('@iota/mam');
+// const Mam = require('@iota/mam');
+const Mam = require('../lib/mam.client.js')
 const { asciiToTrytes, trytesToAscii } = require('@iota/converter');
 
 // Enter root of the
-const root = '';
+if (!process.argv[2]) return console.log('No Address!');
+const root = process.argv[2];
+let nextRoot = root;
+// const root = '';
+
+const provider = 'https://private.tangle.jp:443';
 
 async function initMam() {
   console.log('\r\n\r\n');
   console.log('Listening to MAM stream...');
   console.log('\r\n');
-  await Mam.init('https://nodes.devnet.iota.org:443');
+  await Mam.init(provider);
 }
 
 // Check the MAM stream every 5 seconds for new data on the current root
@@ -17,6 +23,11 @@ async function checkMam() {
   if (root !== nextRoot) {
     root = nextRoot;
   }
+
+  const showData = raw => {
+    const data = trytesToAscii(raw);
+    console.log(data);
+  };
 
   // The showData callback will be called in order for each message found
   const data = await Mam.fetch(root, 'public', null, showData);
